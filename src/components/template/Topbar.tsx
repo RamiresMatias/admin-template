d750bfc6c0916cdce5c6876c0e28aa6e4c606f88
RamiresMatias <ramiresmatias20000@gmail.com>
2022-02-08 21:05:21 -0300

    Add primeiro commit do projeto

---
+++ src/components/template/Topbar.tsx
@@ -0,0 +1,14 @@
+import Title from './Title' 
+
+interface TopbarProps {
+    title: string,
+    subtitle: string,
+}
+
+export default function Topbar(props: TopbarProps) {
+    return (
+        <div>
+            <Title title={props.title} subtitle={props.subtitle}></Title>
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout"
+
+export default function Home() {
+  return (
+    <Layout title="Página Inicial" subtitle="Template Admin">
+      <h3>Conteúdo</h3>
+    </Layout>
+  )
+}
